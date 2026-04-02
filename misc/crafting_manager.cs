d37bdd2736244de9c67b941ae0d2511c3b91908a
Lying <7865059+Lyinginbedmon@users.noreply.github.com>
2026-04-03 00:35:02 +0100

    Update crafting_manager.cs
    Fixed Prototech Scrap not being recognised during inventory scans (still not sure why it wasn't already). Added more configurable options for end-user customisation.

--- misc/crafting_manager.cs
+++ misc/crafting_manager.cs
@@ -1,11 +1,27 @@
 // == Creation Crafting Management OS ==
+
+// HOW TO USE
+// Compile once to populate the programmable block's CustomData
+// Adjust the config however you like
+// * Prefix - Used to indicate blocks being manipulated by this system
+// * CraftRate - How frequently the system updates its crafting requests (higher = less frequently)
+// * StorageFlag - The value in a block's CustomData that flags it as monitored storage
+// * CrafterFlag - The value in an assembler's CustomData that flags it as useable crafting
+// * Thresholds - Self-explanatory, how much of each item to maintain in storage via crafting. Set a given threshold to 0 to have crafting ignore it.
+// Go to each cargo container you want to be included, and put the StorageFlag value in its CustomData (main_hold by default)
+// Go to each assembler you want to be included, and put the CraftFlag value in its CustomData (auto_assembler by default)
+// Then recompile the programmable block to apply the updates
+// For display screens, set their CustomData to comp_0, comp_1, proto, or resources as appropriate and recompile
+
 const String version = "1.0";
 const int INT_MAX = 2147483647;
 const String spinning = "-\\|/";
 const String config_default = 
-    "[general]\nPrefix=CRT\nCraftRate=10\n\n[component_a_thresholds]\nSteelPlate=100000\nInteriorPlate=100000\nConstruction=50000\nSmallTube=10000\nLargeTube=10000\nGirder=10000\n\n[component_b_thresholds]\nMetalGrid=1000\nSolarCell=1000\nDisplay=1000\nPowerCell=1000\nMotor=1000\nComputer=1000\n\n[prototech_thresholds]\nPrototechCapacitor=100\nPrototechCircuitry=100\nPrototechCoolingUnit=100\nPrototechFrame=20\nPrototechMachinery=100\nPrototechPanel=200\nPrototechPropulsionUnit=100\nPrototechScrap=0";
+    "[general]\nPrefix=CRT\nCraftRate=10\nStorageFlag=main_hold\nCrafterFlag=auto_assembler\n\n[component_a_thresholds]\nSteelPlate=100000\nInteriorPlate=100000\nConstruction=50000\nSmallTube=10000\nLargeTube=10000\nGirder=10000\n\n[component_b_thresholds]\nMetalGrid=1000\nSolarCell=1000\nDisplay=1000\nPowerCell=1000\nMotor=1000\nComputer=1000\n\n[prototech_thresholds]\nPrototechCapacitor=100\nPrototechCircuitry=100\nPrototechCoolingUnit=100\nPrototechFrame=20\nPrototechMachinery=100\nPrototechPanel=200\nPrototechPropulsionUnit=100\nPrototechScrap=0";
 
 String prefix;
+String holdFlag = "main_hold";
+String craftFlag = "auto_assembler";
 int craftingRate;
 // All LCD panels
 Dictionary<ScreenType, List<IMyTextPanel>> screenMap = new Dictionary<ScreenType, List<IMyTextPanel>>();
@@ -71,7 +87,6 @@ private static MyItemType[] components_p = new MyItemType[]
         MyItemType.MakeComponent("PrototechPropulsionUnit"),
         MyItemType.MakeComponent("PrototechScrap")
     };
-// FIXME Known bug: system fails to identify PrototechScrap in inventories, resulting in constant crafting while threshold > 0, only item with this issue
 
 // Dictionary of monitored components to their configured crafting thresholds
 private static Dictionary<MyItemType,int> componentThresholds = new Dictionary<MyItemType, int>();
@@ -126,40 +141,10 @@ public Program()
         }
     
     // Identify monitored cargo inventories
-    List<IMyCargoContainer> cargo = new List<IMyCargoContainer>();
-    GridTerminalSystem.GetBlocksOfType<IMyCargoContainer>(cargo);
-    int boxes = 0;
-    foreach(var box in cargo)
-        if(box.CubeGrid == Me.CubeGrid && box.CustomData.ToLower() == "main_hold")
-        {
-            box.CustomName = prefix+" Cargo Hold "+(++boxes);
-            box.ShowInTerminal = false;
-            box.ShowInToolbarConfig = false;
-            containers.Add(box);
-        }
+    collectContainers();
     
     // Identify nominated assemblers
-    List<IMyAssembler> crafters = new List<IMyAssembler>();
-    GridTerminalSystem.GetBlocksOfType<IMyAssembler>(crafters);
-    boxes = 0;
-    foreach(var ass in crafters)
-        if(ass.CubeGrid == Me.CubeGrid && ass.CustomData.ToLower() == "auto_assembler")
-        {
-            string type = "Assembler";
-            string subtype = ass.BlockDefinition.SubtypeName;
-            if(subtype.Contains("Prototech"))
-                type = "Prototech Assembler";
-            else if(subtype.Contains("Basic"))
-                type = "Basic Assembler";
-            // Never use survival kits for autocrafting
-            else if(subtype.Contains("Survival"))
-                continue;
-            
-            ass.CustomName = prefix+" Managed "+type+" "+(++boxes);
-            ass.ShowInTerminal = false;
-            ass.ShowInToolbarConfig = false;
-            assemblers.Add(ass);
-        }
+    collectCrafters();
 }
 
 public void loadConfig(String customData)
@@ -179,6 +164,8 @@ public void loadConfig(String customData)
     
     prefix = config.Get("general", "Prefix").ToString("CRT");
     craftingRate = config.Get("general", "CraftRate").ToInt32(10);
+    holdFlag = config.Get("general", "StorageFlag").ToString("main_hold").ToLower();
+    craftFlag = config.Get("general", "CrafterFlag").ToString("auto_assembler").ToLower();
     
     foreach(MyItemType item in components_0)
         componentThresholds[item] = config.Get("component_a_thresholds", item.SubtypeId).ToInt32(0);
@@ -256,9 +243,10 @@ public void handleInvScreens(List<IMyTextPanel> screens, MyItemType[] listedItem
 
 public void updateAutoCrafting()
 {
-    // Dictionary of items currently queued up in monitored assemblers
+    // Dictionary of items that need to be queued up for crafting
     Dictionary<MyItemType,int> craftingQueue = new Dictionary<MyItemType,int>();
-    
+
+    // Calculate excess crafting needs, by subtracting demand from existing queues
     foreach(MyItemType product in componentRequests.Keys)
     {
         int tally = 0;
@@ -285,11 +273,13 @@ public void updateAutoCrafting()
 
 // #### UTILITY FUNCTIONS ####
 
+// Returns the spinning progress icon
 public char getSpinning()
 {
     return spinning[this.ticksRunning % spinning.Length];
 }
 
+// Reduces a numerical value to a truncated version, such as 1k or 3m
 public string abbreviateValue(int value)
 {
     if(value == 0F)
@@ -310,6 +300,7 @@ public ScreenType getScreenFromString(String value)
     return ScreenType.BLANK;
 }
 
+// Adds the given string to all given screens
 public void echoToScreens(List<IMyTextPanel> screens, string text)
 {
     addStringToDisplays(screens, text, hasClearedScreen);
@@ -322,28 +313,37 @@ public void addStringToDisplays(List<IMyTextPanel> displays, string text, bool a
         screen.WriteText((append ? "\n" : "") + text, append);
 }
 
-private float clamp(float val, float min, float max)
-{
-    float minVal = Math.Min(min, max);
-    float maxVal = Math.Max(min, max);
-    return Math.Max(minVal, Math.Min(maxVal, val));
-}
-
+// Tallies the given item across all monitored inventories
 public int getTotalOfItem(MyItemType item)
 {
     int tally = 0;
     // Items in monitored storage
     foreach(var box in containers)
-        tally += box.GetInventory().GetItemAmount(item).ToIntSafe();
+        tally += getItemAmount(box.GetInventory(), item);
     // Items in input/output of managed assemblers
     foreach(var box in assemblers)
+        tally += getItemAmount(box.InputInventory, item) + getItemAmount(box.OutputInventory, item);
+    return tally;
+}
+
+// Manually compares items by ID, because Prototech Scrap gets overlooked otherwise for some reason
+public int getItemAmount(IMyInventory inv, MyItemType item)
+{
+    int tally = 0;
+    int index = 0;
+    while(index < inv.ItemCount)
     {
-        tally += box.InputInventory.GetItemAmount(item).ToIntSafe();
-        tally += box.OutputInventory.GetItemAmount(item).ToIntSafe();
+        MyInventoryItem? slot = inv.GetItemAt(index);
+        MyInventoryItem contents = slot.Value;
+        
+        if(contents.Type.SubtypeId.Contains(item.SubtypeId))
+            tally += contents.Amount.ToIntSafe();
+        index++;
     }
     return tally;
 }
 
+// Returns true if the given request is of the given item
 public bool isRequestOfItem(MyProductionItem request, MyItemType item)
 {
     return request.BlueprintId.ToString().Contains(item.SubtypeId);
@@ -366,6 +366,7 @@ public MyDefinitionId createRequest(MyItemType item)
     return MyDefinitionId.Parse("MyObjectBuilder_BlueprintDefinition/" + name);
 }
 
+// Provides the given item to the nominated assembler with the shortest queue
 public void pushRequest(MyItemType item, decimal count)
 {
     MyDefinitionId request = createRequest(item);
@@ -394,4 +395,60 @@ public void pushRequest(MyItemType item, decimal count)
         Echo("No viable assembler available for "+item.SubtypeId);
     else
         recipient.AddQueueItem(request, count);
+}
+
+// Collects all nominated cargo containers, as well as all cargo terminals and connectors, on this grid
+public void collectContainers()
+{
+    List<IMyCargoContainer> cargo = new List<IMyCargoContainer>();
+    GridTerminalSystem.GetBlocksOfType<IMyCargoContainer>(cargo);
+    int boxes = 0;
+    foreach(var box in cargo)
+        if(box.CubeGrid == Me.CubeGrid)
+        {
+            string type = box.BlockDefinition.SubtypeName;
+            if(box.CustomData.ToLower() == holdFlag)
+            {
+                if(type.Contains("Container"))
+                    box.CustomName = prefix+" Cargo Hold "+(++boxes);
+                containers.Add(box);
+            }
+            else if(type.Contains("CargoTerminal"))
+                containers.Add(box);
+            
+            box.ShowInTerminal = false;
+            box.ShowInToolbarConfig = false;
+        }
+    
+    List<IMyShipConnector> connectors = new List<IMyShipConnector>();
+    GridTerminalSystem.GetBlocksOfType<IMyShipConnector>(connectors);
+    foreach(var box in connectors)
+        if(box.CubeGrid == Me.CubeGrid)
+            containers.Add(box);
+}
+
+// Collects all nominated assemblers on this grid
+public void collectCrafters()
+{
+    List<IMyAssembler> crafters = new List<IMyAssembler>();
+    GridTerminalSystem.GetBlocksOfType<IMyAssembler>(crafters);
+    int boxes = 0;
+    foreach(var ass in crafters)
+        if(ass.CubeGrid == Me.CubeGrid && ass.CustomData.ToLower() == craftFlag)
+        {
+            string type = "Assembler";
+            string subtype = ass.BlockDefinition.SubtypeName;
+            if(subtype.Contains("Prototech"))
+                type = "Prototech Assembler";
+            else if(subtype.Contains("Basic"))
+                type = "Basic Assembler";
+            // Never use survival kits for autocrafting
+            else if(subtype.Contains("Survival"))
+                continue;
+            
+            ass.CustomName = prefix+" Managed "+type+" "+(++boxes);
+            ass.ShowInTerminal = false;
+            ass.ShowInToolbarConfig = false;
+            assemblers.Add(ass);
+        }
 }
